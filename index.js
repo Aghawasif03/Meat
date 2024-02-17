4efcd28dedc550fabfd174da64b448710cd073aa
Agha Wasif <aghawasif03@gmail.com>
2024-02-17 18:02:02 +0500

    Meat

---
+++ index.js
@@ -0,0 +1,88 @@
+// Three type of javascript
+// 1)Console
+// 2)Window
+// 3)DOM
+
+// 1)
+// console.log("Hi my name is wasif");
+// console.warn("Name is not define");
+// console.error("What is not define");
+
+// 2)
+// alert("Hi agha Wasif")
+// confirm("Are you sure you delte this record")
+
+// 3)
+// a = 2*3;
+// b = 2*4;
+// z = 2*10;
+
+
+
+// document.write(`2 x 2 = ${2*2}`);
+
+// document.write("2 x 3 = "+a);
+// document.write('2 x 4 = ',b);
+// document.write(`2 x 5 = ${2*5}`);
+// document.write(`2 x 6 = ${2*6}`);
+// document.write(`2 x 7 = ${2*7}`);
+// document.write(`2 x 8 = ${2*8}`);
+// document.write(`2 x 9 = ${2*9}`);
+// document.write(`2 x 10 =` +z);
+
+
+
+
+// document.write('Agha wasif 2 jama 2 kitna hota ha ' ,2*2 ,' hota ha yar');
+// //// May i create a table
+// var  tab;
+// tab = (prompt("Please Enter the table."));
+// document.write(`<b>Write the table of  ${tab}  <br> </b> `);
+
+//     document.write(`<h3> ${tab} x 1 =  ${tab*1}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 2 =  ${tab*2}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 3 =  ${tab*3}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 4 =  ${tab*4}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 5 =  ${tab*5}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 6 =  ${tab*6}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 7 =  ${tab*7}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 8 =  ${tab*8}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 9 =  ${tab*9}  <br> </h3> `);
+//     document.write(`<h3> ${tab} x 10 = ${tab*10} <br> </h3> <hr>`);
+//     document.write(`<h3> ${tab+1} x 11 = ${tab+1*11} <br> </h3>`); //isay samajhna ha
+//     document.write(`<h3> ${tab} x 12 = ${tab*12} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 13 = ${tab*13} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 14 = ${tab*14} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 15 = ${tab*15} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 16 = ${tab*16} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 17 = ${tab*17} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 18 = ${tab*18} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 19 = ${tab*19} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 20 = ${tab*20} <br> </h3> <hr>`);
+//     document.write(`<h3> ${tab} x 21 = ${tab*21} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 22 = ${tab*22} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 23 = ${tab*23} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 24 = ${tab*24} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 25 = ${tab*25} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 26 = ${tab*26} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 27 = ${tab*27} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 28 = ${tab*28} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 29 = ${tab*29} <br> </h3>`);
+//     document.write(`<h3> ${tab} x 30 = ${tab*30} <br> </h3>`);
+ 
+
+    var  gm;
+    gm = (prompt("Please specify how many grams you need. 1000 grams meat cost 500Rs here"));
+    document.write(` You have to pay<b> ${0.5*gm} </b>rupees for <b>${gm}gm </b> of chicken.   <br>  `);
+        document.write(`  <br> `);
+
+
+
+
+
+
+
+
+
+
+
